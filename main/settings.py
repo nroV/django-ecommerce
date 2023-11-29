6d22fac36937c5796f1ccd9e0960aa4b944ecd6f
Sovan panhavorn <nightpp19@gmail.com>
2023-11-29 16:16:00 +0700

    herokuv2

--- main/settings.py
+++ main/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-from django.utils.encoding import force_text
-from django.utils.encoding import force_str
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- main/urls.py
+++ main/urls.py
@@ -27,10 +27,11 @@ from django.conf.urls.static import static
 #     TokenRefreshView,
 # )
 
-from django.urls import re_path
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 from django.contrib.auth import views as auth_view
 
@@ -57,7 +58,11 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    path('social-auth/', include('social_django.urls', namespace='social')),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  
+
+    # path('social-auth/', include('social_django.urls', namespace='social')),
+
+
   
     #  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     #  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
