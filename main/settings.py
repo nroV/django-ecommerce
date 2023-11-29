5a2c2bd272660d6d374c2b2fc62aeb8ed6b41c47
Sovan panhavorn <nightpp19@gmail.com>
2023-11-29 22:08:01 +0700

    herokuv2

--- main/settings.py
+++ main/settings.py
@@ -180,13 +180,13 @@ REST_FRAMEWORK = {
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Replace with your preferred backend
-EMAIL_HOST = 'smtop.gmail.com'  # Replace with your email host
+ # Replace with your preferred backend
+EMAIL_HOST = 'smtp.gmail.com'  # Replace with your email host
 EMAIL_PORT = 587  # Replace with your email port
 EMAIL_USE_TLS = True  # Set to False if your email server doesn't use TLS
 EMAIL_HOST_USER = 'Nightpp19@gmail.com'  # Replace with your email username
-EMAIL_HOST_PASSWORD = 'rqgmmzudxvmkrlwc'  # Replace with your email password\
-
+EMAIL_HOST_PASSWORD = 'vfmwehjcvugapwxv'  # Replace with your email password\
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 
 MEDIA_ROOT = BASE_DIR / "upload"
 MEDIA_URL = '/media/'
